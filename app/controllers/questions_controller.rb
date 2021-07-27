f8d3a72a26c70f7d5b887bcc2c8578349cec0528
Sergio Quiroga <squiroga.on@gmail.com>
2021-07-27 00:02:46 -0400

    Solucion al ejercicio 2 v1.1

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,7 @@
 class QuestionsController < ApplicationController
   def ask
-    @asks = ["I am going to work", "?"]
   end
   def answer
-    @answers = ["Great!", "Silly question, get dressed and go to work!", "I don't care, get dressed and go to work!"]
 
     if params[:question] == "I am going to work"
       @answers = "Great!"
@@ -12,6 +10,6 @@ class QuestionsController < ApplicationController
     else
       @answers = "I don't care, get dressed and go to work!"
     end
-    
+
   end
 end
